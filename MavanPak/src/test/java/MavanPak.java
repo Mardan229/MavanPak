9b04783b0a1f24e7c11b7ac1939b1eade6977be7
shahin <shahinurakter085bd@gmail.com>
2021-06-17 23:04:42 -0400

    Hi Samia. this is my work.

--- MavanPak/src/test/java/MavanPak.java
+++ MavanPak/src/test/java/MavanPak.java
@@ -3,6 +3,22 @@ public class MavanPak {
 
 	public static void main(String[] args) {
 System.out.println("Hi Shahin How are you");
+for (int i=1;i<=50;i++) {
+	if (i%3==0&&i%5==0) {
+		System.out.println("DingDong");
+		
+	}else if (i%3==0) {
+		System.out.println("Ding");
+	}else if(i%5==0) {
+		System.out.println("Dong");
+	}else {
+		System.out.println(i);
+	}
+}
+
+
+
+
 	}
 
 }
